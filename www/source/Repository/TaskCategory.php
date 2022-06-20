f2589e7f85764330c6513f1a83479e3c002efb69
Gabriel Syphan <lucasgabrielpdoliveira@gmail.com>
2022-06-19 23:59:35 -0300

    bug fix

--- www/source/Repository/TaskCategory.php
+++ www/source/Repository/TaskCategory.php
@@ -7,6 +7,6 @@ use CoffeeCode\DataLayer\DataLayer;
 class TaskCategory extends DataLayer {
   
   public function __construct() {
-      parent::__construct("TAB_TASKS_CATEGORIES", ["task_id", "category_id"], 'task_id', false);
+      parent::__construct("TAB_TASKS_CATEGORIES", ["task_id", "category_id"], "id", false);
   }
 }
